e850968e532f3d4df1b71d7cc9a00db6ec14abde
Prathamesh Mahale <121166822+beweme11@users.noreply.github.com>
2024-11-25 13:25:27 +0530

    Update main.py
    added state patttern

--- main.py
+++ main.py
@@ -1,235 +1,171 @@
 import math
 from pygame import mixer
 import pygame
-import math
+from abc import ABC, abstractmethod
 
-
-# intializes the game package
 pygame.init()
 
-# creates the screen
 screen = pygame.display.set_mode((800, 600))
 
-# background
 backimage = pygame.image.load('background.png')
 
-# background music
 mixer.music.load('backgroundmusic.mp3')
 pygame.mixer.music.play(-1)
 
-
-
-
-# title and icon
 pygame.display.set_caption("Mahantariksh ki Jung")
 icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
-# player1 image resizing and loading
-image = pygame.image.load('spaceship.png')
-width = image.get_rect().width
-height = image.get_rect().height
+def load_and_resize_image(path, scale):
+    image = pygame.image.load(path)
+    width = image.get_rect().width
+    height = image.get_rect().height
+    return pygame.transform.scale(image, (width * scale, height * scale))
 
-player1Image = pygame.transform.scale(image, (width * (1 / 10), height * (1 / 10)))
-player1X = 370
-player1Y = 530
-player1Y_change = 0
-player1X_change = 0
-
-player1_score = 0
-player2_score = 0
-
-
-def player1(x, y):
-    screen.blit(player1Image, (x, y))
-
-
-# player2 image resizing and loading
-
-image2 = pygame.image.load('ufo.png')
-width = image2.get_rect().width
-height = image2.get_rect().height
-player2Image = pygame.transform.scale(image2, (width * (1 / 10), height * (1 / 10)))
-player2X = 200
-player2Y = 20
-player2X_change = 0
-player2Y_change = 0
-
-
-def player2(x, y):
-    screen.blit(player2Image, (x, y))
-
-
-# display score on screen
+player1Image = load_and_resize_image('spaceship.png', 1/10)
+player2Image = load_and_resize_image('ufo.png', 1/10)
+bulletImage = load_and_resize_image('bullet.png', 1/15)
+thunderImage = load_and_resize_image('thunder.png', 1/15)
 
 font = pygame.font.Font('god-of-war.ttf', 16)
-etextX = 10
-etextY = 5
-ptextX = 640
-ptextY = 580
 
-def show_player1_score(x, y):
-    player1score = font.render("player 1 score :" + str(player1_score), True, (255, 255, 255))
-    screen.blit(player1score, (x, y))
+class ProjectileState(ABC):
+    @abstractmethod
+    def update(self, projectile):
+        pass
 
+    @abstractmethod
+    def fire(self, projectile):
+        pass
 
-def show_player2_score(x, y):
-    player2score = font.render("player 2 score :" + str(player2_score), True, (255, 255, 255))
-    screen.blit(player2score, (x, y))
+class ReadyState(ProjectileState):
+    def update(self, projectile):
+        projectile.y = projectile.initial_y
 
-
-# bullet image loading and resizing
-image3 = pygame.image.load('bullet.png')
-width = image3.get_rect().width
-height = image3.get_rect().height
-bulletImage = pygame.transform.scale(image3, (width * (1 / 15), height * (1 / 15)))
-bulletX = 0
-bulletY = 480
-bulletX_change = 0
-bulletY_change = 1.2
-# state= fire means in action and visible, ready means not in action and invisible
-bullet_state = "ready"
-
-# thunder image loading and resizing
-image4 = pygame.image.load('thunder.png')
-width = image4.get_rect().width
-height = image4.get_rect().height
-thunderImage = pygame.transform.scale(image4, (width * (1 / 15), height * (1 / 15)))
-thunderX = 0
-thunderY = 20
-thunderX_change = 0
-thunderY_change = -1.2
-
-# state= fire means in action and visible, ready means not in action and invisible
-thunder_state = "ready"
-
-
-def fire_bullet(x, y):
-    global bullet_state
-    bullet_state = "fire"
-    screen.blit(bulletImage, (x + 8, y + 25))
-
-
-def fire_thunder(x, y):
-    global thunder_state
-    thunder_state = "fire"
-    screen.blit(thunderImage, (x, y))
-
-
-def collision(player2X, player2Y, player1X, player1Y):
-    distance = math.sqrt(math.pow((player2X - bulletX), 2)) + (math.pow((player2Y - bulletY), 2))
-
-    if distance < 27:
+    def fire(self, projectile):
+        projectile.state = FiringState()
+        projectile.x = projectile.owner.x
         return True
-    else:
+
+class FiringState(ProjectileState):
+    def update(self, projectile):
+        projectile.y += projectile.speed
+        if projectile.y < -25 or projectile.y > 600:
+            projectile.state = ReadyState()
+
+    def fire(self, projectile):
         return False
 
+class Projectile:
+    def __init__(self, image, owner, speed, initial_y):
+        self.image = image
+        self.owner = owner
+        self.speed = speed
+        self.initial_y = initial_y
+        self.x = 0
+        self.y = initial_y
+        self.state = ReadyState()
 
-def collison2():
-    distance2 = math.sqrt(math.pow((player1X - thunderX), 2)) + (math.pow((player1Y - thunderY), 2))
+    def update(self):
+        self.state.update(self)
 
-    if distance2 < 27:
-        return True
-    else:
-        return False
+    def fire(self):
+        return self.state.fire(self)
 
-    # game window infinite loop
+    def draw(self, screen):
+        if isinstance(self.state, FiringState):
+            screen.blit(self.image, (self.x, self.y))
 
+class Player:
+    def __init__(self, image, x, y):
+        self.image = image
+        self.x = x
+        self.y = y
+        self.score = 0
+        self.x_change = 0
 
-# game loop
+    def move(self):
+        self.x += self.x_change
+        if self.x > 800:
+            self.x = 0
+        if self.x < 0:
+            self.x = 800
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+
+player1 = Player(player1Image, 370, 530)
+player2 = Player(player2Image, 200, 20)
+
+bullet = Projectile(bulletImage, player1, -1.2, 480)
+thunder = Projectile(thunderImage, player2, 1.2, 20)
+
+def show_score(screen, font, player, player_num, x, y):
+    score_text = font.render(f"Player {player_num} score: {player.score}", True, (255, 255, 255))
+    screen.blit(score_text, (x, y))
+
+def check_collision(projectile, target):
+    distance = math.sqrt((target.x - projectile.x)**2 + (target.y - projectile.y)**2)
+    return distance < 27
+
 running = True
 while running:
     screen.fill((0, 0, 0))
     screen.blit(backimage, (0, 0))
-    show_player2_score(etextX, etextY)
-    show_player1_score(ptextX, ptextY)
+    show_score(screen, font, player2, 2, 10, 5)
+    show_score(screen, font, player1, 1, 640, 580)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-        # check if key is pressed and change position of player1
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                player1X_change = -0.3
+                player1.x_change = -1
             if event.key == pygame.K_RIGHT:
-                player1X_change = +0.3
-
-        if event.type == pygame.KEYDOWN:
+                player1.x_change = 1
             if event.key == pygame.K_a:
-                player2X_change = -0.3
+                player2.x_change = -1
             if event.key == pygame.K_d:
-                player2X_change = +0.3
-
-        if event.type == pygame.KEYDOWN:
+                player2.x_change = 1
             if event.key == pygame.K_UP:
-                if bullet_state == "ready":
-                    bulletX = player1X
-                    fire_bullet(bulletX, bulletY)
-                bullet_sound = mixer.Sound('bullet_sound.wav')
-                bullet_sound.play()
-
+                if bullet.fire():
+                    bullet_sound = mixer.Sound('bullet_sound.wav')
+                    bullet_sound.play()
             if event.key == pygame.K_w:
-                if thunder_state == "ready":
-                    thunderX = player2X
-                    fire_thunder(thunderX, thunderY)
-                thunder_sound = mixer.Sound('thunder_sound.wav')
-                thunder_sound.play()
+                if thunder.fire():
+                    thunder_sound = mixer.Sound('thunder_sound.wav')
+                    thunder_sound.play()
+        if event.type == pygame.KEYUP:
+            if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
+                player1.x_change = 0
+            if event.key in (pygame.K_a, pygame.K_d):
+                player2.x_change = 0
 
-    if bullet_state is "fire":
-        fire_bullet(bulletX, bulletY)
-        bulletY -= bulletY_change
+    player1.move()
+    player2.move()
+    bullet.update()
+    thunder.update()
 
-    if thunder_state is "fire":
-        fire_thunder(thunderX, thunderY)
-        thunderY -= thunderY_change
-
-    # repositioning to extremes
-    if bulletY < -25:
-        bullet_state = "ready"
-        bulletY = player1Y - 10
-    if thunderY > 600:
-        thunder_state = "ready"
-        thunderY = player2Y + 10
-    if player1X > 800:
-        player1X = 0
-    if player1X < 0:
-        player1X = 800
-
-    if player2X > 800:
-        player2X = 0
-    if player2X < 0:
-        player2X = 800
-
-    # collision
-    hascollision = collision(player2X, player2Y, bulletX, bulletY)
-    if hascollision:
+    if check_collision(bullet, player2):
         explosion_sound = mixer.Sound('explosion.wav')
         explosion_sound.play()
-        bulletY = player1Y - 10
-        bullet_state = "ready"
-        player1_score += 1
+        bullet.state = ReadyState()
+        player1.score += 1
 
-
-    # collison 2
-    hascollision2 = collison2()
-    if hascollision2:
+    if check_collision(thunder, player1):
         explosion_sound = mixer.Sound('explosion.wav')
         explosion_sound.play()
-        thunderY = player2Y + 10
-        thunder_state = "ready"
-        player2_score += 1
+        thunder.state = ReadyState()
+        player2.score += 1
 
-
-    player1X += player1X_change
-    player1Y += player1Y_change
-
-    player1(player1X, player1Y)
-
-    player2X += player2X_change
-    player2Y += player2Y_change
-
-    player2(player2X, player2Y)
+    player1.draw(screen)
+    player2.draw(screen)
+    bullet.draw(screen)
+    thunder.draw(screen)
 
     pygame.display.update()
-print("player1 score is : ", player1_score)
-print("player2 score is : ", player2_score)
+
+print("Player 1 score is:", player1.score)
+print("Player 2 score is:", player2.score)
+
+pygame.quit()
